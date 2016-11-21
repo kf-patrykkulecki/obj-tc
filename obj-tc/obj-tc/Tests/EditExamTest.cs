1a4565466829d0a867a8a3b82be370c003e76acb
kf-patrykkulecki <pkulecki@objectivity.co.uk>
2016-11-21 11:26:33 +0100

    negative path to change from per exam type to per session

--- obj-tc/obj-tc/Tests/EditExamTest.cs
+++ obj-tc/obj-tc/Tests/EditExamTest.cs
@@ -295,6 +295,64 @@ namespace obj_tc.Tests
             sessionDetailsPage.ExamsSpaceBasic.Should().Be(100);
         }
 
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(1000)]
+        public void EditExamSessionInOrderToChangesPerExamTypeToPerSessionNegative_Test(int param)
+        {
+            var landingPage = new LandingPage(DriverContext);
+
+            // Data
+            var sessionDate = date.AddDays(1).ToString(format);
+            var sessionCity = GetTimeStamp();
+            var level = new List<string>
+            {
+                "Podstawowy",
+                "Ekspercki"
+            };
+            var product = new Dictionary<string, int>
+            {
+                {"ISTQB Foundation Level / Polski, Angielski", 999},
+                {"ISTQB Improving the Testing Process / Angielski", 999 }
+            };
+            var expectedProduct = new List<string>
+            {
+                "ISTQB Foundation Level/Polski, Angielski",
+                "ISTQB Improving the Testing Process/Angielski"
+            };
+
+            var addSessionPage =
+                landingPage.OpenLandingPage().OpenLogInPage().SetEmail(email).SetPassword(password).LogIn().Topbar.OpenAddSession();
+
+            // Create session
+            addSessionPage.SetDate(sessionDate)
+                .SetCity(sessionCity.ToString())
+                .SelectSpacePerProduct()
+                .SelectLevel(level)
+                .SelectProduct(product.Select(el => el.Key).ToList());
+
+            foreach (var prod in product)
+            {
+                addSessionPage.SetProductSpace(prod.Key, prod.Value);
+            }
+
+            var sessionDetailsPage = addSessionPage.SaveSession();
+
+            // Check session details
+            sessionDetailsPage.Space.Should().Be(product.Select(el => el.Value).Sum());
+            sessionDetailsPage.SwitchToExams();
+            sessionDetailsPage.ExamList.ShouldAllBeEquivalentTo(expectedProduct);
+            sessionDetailsPage.ExamsSpaceBasic.Should().Be(999);
+            sessionDetailsPage.ExamsSpaceExpert.Should().Be(999);
+
+            // Check if session present on dashboard
+            sessionDetailsPage.EditSession();
+
+            addSessionPage.SelectSpacePerSession().SetSpacePerSession(param.ToString()).SaveSessionReturnAddSession();
+            addSessionPage.spacePerSessionValidationPresent.Should().BeTrue();
+            addSessionPage.spacePerSessionValidationText.Should().Be("Wymagana jest liczba całkowita z zakresu 0-999");
+        }
+
         private int GetTimeStamp()
         {
             return (int)(date.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
